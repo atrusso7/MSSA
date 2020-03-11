a3449333e4fd76de4ce82cdd0507ebded50ba0e1
Anthony Russo <atrusso7@gmail.com>
2020-03-11 11:30:22 -0500

    11 Mar

--- ASP.NET/11Mar_movie/Controllers/HelloWorldController.cs
+++ ASP.NET/11Mar_movie/Controllers/HelloWorldController.cs
@@ -16,9 +16,9 @@ namespace _11Mar_movie
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        public string Welcome(string name, int ID = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
