1d95a452643238b0f3e9dda5ac1a24f03e8818f5
Anthony Russo <atrusso7@gmail.com>
2020-04-15 11:31:52 -0500

    Delegates

--- ISTA421/Apr15_Delegates/Apr15_Delegates/Program.cs
+++ ISTA421/Apr15_Delegates/Apr15_Delegates/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Apr15_Delegates
 {
@@ -6,7 +9,54 @@ namespace Apr15_Delegates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DelMethod D1 = DelP.Show;
+            DelMethod D2 = new DelMethod(DelP.Display);
+            DelMethod D3 = new DelMethod(DelP.Print);
+            D1();
+            D2();
+            D3();
+            
+            //Part 2
+            AddNum a1 = SumSub.Sum;
+            SubNum s1 = SumSub.Subtract;
+            s1(5, 10);
+            a1(5, 10);
+            Console.ReadLine();
+        }
+        public delegate void DelMethod();
+
+        public delegate void AddNum(int a, int b);
+
+        public delegate void SubNum(int a, int b);
+
+        class DelP
+        {
+            public static void Display()
+            {
+                Console.WriteLine("Display");
+            }
+
+            public static void Show()
+            {
+                Console.WriteLine("Show");
+            }
+
+            public static void Print()
+            {
+                Console.WriteLine("Print");
+            }
+        }
+    }
+
+    class SumSub
+    {
+        public static void Sum(int a, int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+        public static void Subtract(int a, int b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
         }
     }
 }
